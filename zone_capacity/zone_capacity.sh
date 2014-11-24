65f708fadd8f872b2502ea430274d5ea62106dd3
Justin Mammarella <jmamma@gmail.com>
2014-11-24 14:15:45 +1100

    Updated documentation

--- zone_capacity/zone_capacity.sh
+++ zone_capacity/zone_capacity.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-rm ./tmp
-rm ./tmp3
-rm ./tmp2
-rm ./tmp4
-rm ./tmp5
-rm ./flavors
-HOST=np-rcc9
-totalcores=0
-totalmem=0
+# Justin Mammarella 24/11/2014
+# The script will scan OpenStack compute nodes and report on their current resource usage.
+#
+# Information pertaining to VMs is gathered using NOVA commands. From this, VM flavour and state is obtained.
+# Physical information about node hardware is obtained by examining /proc/cpuinfo /proc/meminfo on nodes via ssh.
+
+# A file containg a list of all node hostnames must be provided.
+# Hosts must be accessible via SSH and keys verified.
+
+
+rm -f ./flavors
+rm -f ./tmp
+
+HOST=np-rcc50
+
+#Retrieve Flavour information 
 
 nova flavor-list | tail -n 6 | head -n 5 >> tmp
 
-#Store Flavour Infomration in memory
+#Store Flavour Infomration in memory for retrieval later
 
 cat tmp | while read line; do
     var=$(echo $line | cut -f3 -d'|' | tr -d ' ' | tr -d '.' )
@@ -21,51 +28,95 @@ cat tmp | while read line; do
     echo "$var $mem $cpu" >> flavors
 done 
 
-#nova list --host np-rcc5 --all-tenants
 
-nova list --host $HOST --all-tenants >> tmp2
-n=`expr $(cat tmp2 | wc -l) - 3`
-cat tmp2 | tail -n $n | head -n -1 >> tmp3
+#For Each Host get VM information:
 
-filename='tmp3'
-exec 4<$filename
+hostlist='/home/jmammarella/openstack/nodes/np-rcc'
 
-while read -u4 line; do
+exec 3<$hostlist
 
-    id=$(echo $line | cut -f2 -d '|') 
+while read -u3 HOST; do
 
-    flav=`nova show $id | grep flavor | cut -f3 -d'|' | cut -f1 -d'(' | tr -d ' ' | tr -d '.'`
-   # cat flavors | grep $flav
-    mem=$(cat flavors | grep $flav | cut -f2 -d' ' | tr -d ' ' )  
-    cores=$(cat flavors | grep $flav | cut -f3 -d' ' | tr -d ' ')  
+    totalcores=0
+    totalmem=0
+
+    rm -f ./tmp3
+    rm -f ./tmp2
+    rm -f ./tmp4
+    rm -f ./tmp5
     
-    totalcores=`expr $cores + $totalcores`
-    totalmem=`expr $mem + $totalmem`
+    #For Each VM calculate resource usage.
 
-done
+    #nova list --host np-rcc5 --all-tenants
 
-echo "Host: $HOST TotalCoresInUseByVM: $totalcores TotalMemInUseByVM: $totalmem"
+    #Get a list of VMs running on the Node.
+    
+    nova list --host $HOST --all-tenants >> tmp2
+    n=`expr $(cat tmp2 | wc -l) - 3`
+    cat tmp2 | tail -n $n | head -n -1 >> tmp3
 
-ssh $HOST "cat /proc/cpuinfo" >> tmp4
+    filename='tmp3'
+    exec 4<$filename
 
+    #Itterate over VM list of Nodes, extracting ID and State.
 
-filename='tmp4'
-exec 5<$filename
-cores=0
-while read -u5 line; do
+    while read -u4 line; do
+
+        id=$(echo $line | cut -f2 -d '|') 
+        
+        nova show $id > tmp6
+
+        flav=`cat tmp6 | grep flavor | cut -f3 -d'|' | cut -f1 -d'(' | tr -d ' ' | tr -d '.'`
+        state=`cat tmp6 | grep vm_state | cut -f3 -d'|' | tr -d ' '`
+        
+        #Use flavor information to determine number of cores and memory reserved for VMs.
+
+        if [ "$state" != "suspended" ]; then 
+
+            # cat flavors | grep $flav
+            mem=$(cat flavors | grep $flav | cut -f2 -d' ' | tr -d ' ' )  
+            cores=$(cat flavors | grep $flav | cut -f3 -d' ' | tr -d ' ')  
+    
+            totalcores=`expr $cores + $totalcores`
+            totalmem=`expr $mem + $totalmem`
+        fi
+
+    done
+
+    vm_count=`expr $(cat tmp2 | wc -l) - 4`
+
+    echo "========================================="
+    echo "Host: $HOST"
+    echo "========================================="
+    echo "Number of VMs: $vm_count"
+    echo "Total Cores In Use/Reserved: $totalcores || Total Mem Reserverd By VMs: $totalmem MB"
+
+    ssh $HOST "cat /proc/cpuinfo" >> tmp4
+
+    #Count Physical CPU Cores.
+
+    filename='tmp4'
+    exec 5<$filename
+    cores=0
+    while read -u5 line; do
         
         if [ "$(echo $line | cut -f1 -d':' | tr -d ' ')" = "cpucores" ]; then
                 cores=`expr $cores + $(echo $line | cut -f2 -d':' | tr -d ' ')`  
 
         fi
+    done
+
+    #Get Physical Memory
+    
+    ssh $HOST "cat /proc/meminfo" >> tmp5
+
+    sys_memfree=`expr $(cat tmp5 | grep MemFree: | cut -f2 -d':' | tr -d ' ' | tr -d 'kB') / 1024`
+    sys_memtotal=`expr $(cat tmp5 | grep MemTotal: | cut -f2 -d':' | tr -d ' ' | tr -d 'kB') / 1024`
+
+    echo "Total Cores on Host: $cores || Total Mem Avail on Host:$sys_memtotal MB - Current System Mem Free: $sys_memfree MB"
+    echo "--"
+    echo "Free Cores: $(expr $cores - $totalcores) || Mem Available for New VMs = $(expr $sys_memtotal - $totalmem) MB"
+    #rm flavors
+
 done
-
-ssh $HOST "cat /proc/meminfo" >> tmp5
-
-memfree=$(cat tmp5 | grep MemFree: | cut -f2 -d':' | tr -d ' ')
-memtotal=$(cat tmp5 | grep MemTotal: | cut -f2 -d':' | tr -d ' ')
-
-echo "Host: $HOST Cores: $cores TotalMem: $memtotal CurrentFreeMem: $memfree"
-
-#rm flavors
 
